a3e0d3e01c8922ee10b30e222250dd2baf807ccd
Gourav <gourav@macs-iMac-2.local>
2020-01-21 15:39:26 +0530

    First Commit

---
+++ GoogleAuth/MainPage.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Auth;
+using Xamarin.Forms;
+
+namespace GoogleAuth
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(true)]
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+
+            Auth = new OAuth2Authenticator(
+            Configuration.ClientId,
+            string.Empty,
+            "email",
+            new Uri("https://accounts.google.com/o/oauth2/v2/auth"),
+            new Uri(Configuration.RedirectUrl),
+            new Uri("https://www.googleapis.com/oauth2/v4/token"),
+            isUsingNativeUI: true);
+
+
+            Auth.Completed += OnAuthenticationCompleted;
+            Auth.Error += OnAuthenticationFailed;
+
+        }
+    }
+}
